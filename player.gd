d9bead30c7d202f4e72705ce1249c5d05f68ec2b
nicom7 <7100895+nicom7@users.noreply.github.com>
2023-11-02 18:28:08 -0400

    Update input actions from current platform tone
    - Press next/previous note letter to move right/left, press current note letter to jump. E.g.: On platform C, press B to go left, D to go right and C to jump; On platform A, press G to go left, B to go right and A to jump.

--- player.gd
+++ player.gd
@@ -1,50 +1,73 @@
 extends CharacterBody2D
 
-@export var current_floor_map: TileMap
+@export var speed: float = 300.0
+@export var jump_speed: float = -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var current_floor_map: TileMap
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var floor_map_layer_info: Dictionary = {}
+var _floor_map_layer_info: Dictionary = {}
+
+class InputActions:
+	var move_left: String
+	var move_right: String
+	var jump: String
+
+var _input_actions: Dictionary = {}
+const _tones: Array[String] = ["A", "B", "C"]
+var current_tone: String = "A"
 
 func _ready() -> void:
 	_setup_floor_map_layer_info()
+	_setup_input_actions()
 	
 func _setup_floor_map_layer_info():
-	floor_map_layer_info.clear()
+	_floor_map_layer_info.clear()
 	if current_floor_map:
 		for l in current_floor_map.get_layers_count():
 			var name = current_floor_map.get_layer_name(l)
-			floor_map_layer_info[name] = l
+			_floor_map_layer_info[name] = l
+	
+func _setup_input_actions():
+	_input_actions.clear()
+	for tone in _tones:
+		var actions: InputActions = InputActions.new()
+		actions.move_left = tone + "_left"
+		actions.move_right = tone + "_right"
+		actions.jump = tone + "_jump"
+		
+		_input_actions[tone] = actions
 	
 func _physics_process(delta: float) -> void:
-	_update_controls(delta)
+	_update_tone(delta)
 	_update_movement(delta)
 	
-func _update_controls(delta: float) -> void:
+func _update_tone(delta: float) -> void:
 	var custom_data = _get_floor_custom_data($TileDetector.global_position, "Tones", "Tone")
-	var pitch: String = custom_data if custom_data != null else "-"
-	print(pitch)
+	if custom_data != null:
+		current_tone = custom_data
+	print(current_tone)
 	
 func _update_movement(delta: float) -> void:
+	var current_input_actions: InputActions = _input_actions[current_tone]
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed(current_input_actions.jump) and is_on_floor():
+		velocity.y = jump_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis(current_input_actions.move_left, current_input_actions.move_right)
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
@@ -60,7 +83,7 @@ func _get_floor_custom_data(pos: Vector2, tile_map_layer: String, data_layer: St
 func _get_floor_tile_data(pos: Vector2, layer: String):
 	var tile_data: TileData
 	if current_floor_map:
-		var layer_idx = floor_map_layer_info[layer] if floor_map_layer_info.has(layer) else 0
+		var layer_idx = _floor_map_layer_info[layer] if _floor_map_layer_info.has(layer) else 0
 		var floor_pos = current_floor_map.local_to_map(current_floor_map.to_local(pos))
 		tile_data = current_floor_map.get_cell_tile_data(layer_idx, floor_pos)
 		
