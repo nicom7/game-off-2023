0982fd20ea7ccb65e7f99fd2add435e2fa0859cd
nicom7 <7100895+nicom7@users.noreply.github.com>
2023-11-04 19:05:01 -0400

    Detect switch block hit
    - Play note sequence when block is hit by player

--- game_objects/switch_blocks/switch_block.gd
+++ game_objects/switch_blocks/switch_block.gd
@@ -2,11 +2,16 @@
 class_name SwitchBlock
 extends Node2D
 
+signal hit(block: SwitchBlock)
+
 @export var tone: Globals.Tone = Globals.Tone.A:
 	set(value):
 		if tone != value:
 			tone = value
 			_update_block()
+			
+@export var hit_note_player_scene: PackedScene
+var hit_note_player: NotePlayer
 
 func _update_block() -> void:
 	if not is_node_ready():
@@ -17,9 +22,31 @@ func _update_block() -> void:
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_update_block()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_hit_detection_body_entered(body: Node2D) -> void:
+	var character = body as Character
+	if character and character.velocity.y < 0:
+		hit.emit(self)
+		
+func _on_hit(block: SwitchBlock) -> void:
+	hit_note_player = hit_note_player_scene.instantiate()
+	hit_note_player.tone = tone
+	hit_note_player.octave_ratio = 1
+	add_child(hit_note_player)
+	hit_note_player.start()
+	$NotePlayerTimer.start()
+
+
+func _on_note_player_timer_timeout() -> void:
+	hit_note_player = hit_note_player_scene.instantiate()
+	hit_note_player.tone = tone
+	hit_note_player.octave_ratio = 0.2
+	add_child(hit_note_player)
+	hit_note_player.start()
