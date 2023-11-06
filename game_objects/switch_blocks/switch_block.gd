b0678673dc640202a9dc1e353b5314ffbec961cf
nicom7 <7100895+nicom7@users.noreply.github.com>
2023-11-05 19:10:41 -0500

    Block Sequence game state machine
    - Set block sequence length max to 4
    - Block sequence plays a demo ascending on all it's children blocks, then it setups a random block sequence of max length. Sequence is then played progressively starting from one block to all blocks. After each playback the player must hit blocks in the same order they were played.
    - If the player hits the wrong block, the whole block sequence is reset and player must start over.
    - When the max length sequence is succeeded, the demo is replayed and a new block sequence is setup.

--- game_objects/switch_blocks/switch_block.gd
+++ game_objects/switch_blocks/switch_block.gd
@@ -15,14 +15,26 @@ signal is_hit(block: SwitchBlock)
 @export var block_note_player_scene: PackedScene
 var block_note_player: NotePlayer
 
+var hit_detection_enabled: bool:
+	get:
+		if is_node_ready():		
+			return $HitDetection.monitoring
+		return false
+	set(value):
+		if is_node_ready():
+			$HitDetection.set_deferred("monitoring", value)
+			
+var _cur_hit_valid: bool = false
+
 func hit() -> void:
+	_cur_hit_valid = hit_valid
 	is_hit.emit(self)
 	
 	$AnimationPlayer.stop()
-	$AnimationPlayer.play("hit_valid" if hit_valid else "hit_invalid")
+	$AnimationPlayer.play("hit_valid" if _cur_hit_valid else "hit_invalid")
 	block_note_player = block_note_player_scene.instantiate()
 	block_note_player.tone = tone
-	block_note_player.pitch = 4.0 if hit_valid else 0.7
+	block_note_player.pitch = 4.0 if _cur_hit_valid else 0.7
 	block_note_player.octave_ratio = 0
 	add_child(block_note_player)
 	block_note_player.start()
@@ -53,7 +65,7 @@ func _on_hit_detection_body_entered(body: Node2D) -> void:
 func _on_note_player_timer_timeout() -> void:
 	block_note_player = block_note_player_scene.instantiate()
 	block_note_player.tone = tone
-	block_note_player.pitch = 2.0 if hit_valid else 0.6
+	block_note_player.pitch = 2.0 if _cur_hit_valid else 0.6
 	block_note_player.octave_ratio = 0.2
 	add_child(block_note_player)
 	block_note_player.start()
