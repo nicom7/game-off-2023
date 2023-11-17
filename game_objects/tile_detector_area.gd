f30ffad9309e0ed581bcbbf5075aa0d658e0a055
nicom7 <7100895+nicom7@users.noreply.github.com>
2023-11-16 23:29:35 -0500

    Fix broken world platforms following refactor
    - Adapt tile detector for `Platform` nodes

--- game_objects/tile_detector_area.gd
+++ game_objects/tile_detector_area.gd
@@ -3,15 +3,9 @@ extends Area2D
 signal tone_changed(tone: Globals.Tone)
 
 func _on_body_entered(body: Node2D) -> void:
-	var floor_map: TileMap = body as TileMap
-	if floor_map:
-		# Skip layer 0 (Platform layer)
-		for l in range(1, floor_map.get_layers_count()):
-			if floor_map.is_layer_enabled(l):
-				# Enabled layer - 1 is the current tone
-				var tone = (l - 1) as Globals.Tone
-				tone_changed.emit(tone)
-				break
+	var platform: Platform = body as Platform
+	if platform:
+		tone_changed.emit(platform.tone)
 	else:
 		var switch_block: SwitchBlock = body as SwitchBlock
 		if switch_block:
