65a02df9fb708e3603fb5a771ddd7bc85e035e82
nicom7 <7100895+nicom7@users.noreply.github.com>
2024-04-03 14:07:09 -0400

    Add computer keyboard image helper
    - Encapsulate tone labels into reusable scene
    - Add computer keyboard image with tone overlays

--- gui/tone_label.gd
+++ gui/tone_label.gd
@@ -1,6 +1,6 @@
 @tool
 class_name ToneLabel
-extends Label
+extends CenterContainer
 
 @export var tone: = Globals.Tone.A:
 	set(value):
@@ -15,7 +15,7 @@ func _update_label() -> void:
 		return
 
 	var tone_color = Config.tone_colors[tone]
-	self_modulate = tone_color.lightened(Globals.LIGHT_COLOR_AMOUNT)
+	%Label.self_modulate = tone_color.lightened(Globals.LIGHT_COLOR_AMOUNT)
 	var label = Globals.get_label_from_tone(tone)
 
-	text = label
+	%Label.text = label
