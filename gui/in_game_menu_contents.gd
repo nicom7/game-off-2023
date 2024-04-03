bce0e683b8c7ad8646639a8b0b2f182d019b836b
nicom7 <7100895+nicom7@users.noreply.github.com>
2024-04-03 14:39:33 -0400

    Help menu integration
    - Implement Help menu contents with tab container for displaying piano and keyboard layout images
    - Add Help menu in IGM and connect button signals
    - Add keyboard image license in credits menu

--- gui/in_game_menu_contents.gd
+++ gui/in_game_menu_contents.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 signal settings_pressed()
+signal help_pressed()
 signal credits_pressed()
 signal exit_pressed()
 signal close_pressed()
@@ -8,6 +9,10 @@ signal close_pressed()
 
 func _on_settings_btn_pressed() -> void:
 	settings_pressed.emit()
+
+
+func _on_help_btn_pressed() -> void:
+	help_pressed.emit()
 
 
 func _on_credits_btn_pressed() -> void:
@@ -20,3 +25,4 @@ func _on_exit_btn_pressed() -> void:
 
 func _on_close_menu_btn_pressed() -> void:
 	close_pressed.emit()
+
